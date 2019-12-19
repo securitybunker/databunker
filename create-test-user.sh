8b1e1d254fe03c19fb73f94403dda2a5122f4830
stremovsky <stremovsky@gmail.com>
2019-12-19 12:58:01 +0200

    Add more exampkes for user app API

--- create-test-user.sh
+++ create-test-user.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-DATABUNKER_APIKEY=$1
-if [ -z $DATABUNKER_APIKEY ]; then
+XTOKEN=$1
+if [ -z $XTOKEN ]; then
   echo "missing api key parameter"
   exit
 fi
 
 echo "Creating user."
 RESULT=`curl -s http://localhost:3000/v1/user \
-  -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json" \
+  -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
   -d '{"fname":"Test","lname":"Account","email":"test@paranoidguy.com","phone":"4444"}'`
 STATUS=`echo $RESULT | jq ".status" | tr -d '"'`
 if [ "$STATUS" == "error" ]; then
   echo "Error to create user, trying to lookup by email."
-  RESULT=`curl -s http://localhost:3000/v1/user/email/test@paranoidguy.com -H "X-Bunker-Token: "$DATABUNKER_APIKEY`
+  RESULT=`curl -s http://localhost:3000/v1/user/email/test@paranoidguy.com -H "X-Bunker-Token: "$XTOKEN`
   STATUS=`echo $RESULT | jq ".status" | tr -d '"'`
 fi
 if [ "$STATUS" == "error" ]; then
@@ -25,12 +25,22 @@ TOKEN=`echo $RESULT | jq ".token" | tr -d '"'`
 echo "User token is $TOKEN"
 
 RESULT=`curl -s http://localhost:3000/v1/userapp/token/$TOKEN/shipping \
-  -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json" \
-  -d '{"country":"Israel","address":"Allenby 1","postcode":"12345","status":"active"}' | jq ".status" | tr -d '"'`
+  -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
+  -d '{"country":"UK","city":"London","address":"221B Baker Street","postcode":"12345","status":"new"}'`
 echo "User shipping record created, status $RESULT"
 
+RESULT=`curl -s http://localhost:3000/v1/userapp/token/$TOKEN/shipping -XPUT \
+  -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
+  -d '{"status":"delivered"}'`
+echo "User shipping record updated, status $RESULT"
+
+RESULT=`curl -s http://localhost:3000/v1/userapp/token/$TOKEN/shipping \
+  -H "X-Bunker-Token: "$XTOKEN`
+echo "User shipping record ready, status $RESULT"
+
+
 RESULT=`curl -s http://localhost:3000/v1/sharedrecord/token/$TOKEN \
-  -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json" \
+  -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
   -d '{"app":"shipping","fields":"address"}'`
 echo "Shared record created, status $RESULT"
 RECORD=`echo $RESULT | jq ".record" | tr -d '"'`
@@ -40,39 +50,39 @@ RESULT=`curl -s http://localhost:3000/v1/get/$RECORD`
 echo "Get shared record (no password/access token): $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/userapp/token/$TOKEN \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "View list of all user apps $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/userapps \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "View list of all apps $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/token/$TOKEN/send-sms -XPOST \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "Enable consent send-sms for user by token: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/email/test@paranoidguy.com/send-sms2 -XPOST \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "Enable consent send-sms for user by email: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/phone/4444/send-sms2 -XDELETE \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "Withdraw consent send-sms for user by phone: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/token/$TOKEN/send-sms \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "View this specific consent only: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/token/$TOKEN \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "View all user consents: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consents/send-sms \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "View all users with send-sms consent on: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/session/token/$TOKEN -XPOST \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json" \
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
    -d '{"clientip":"1.1.1.1","x-forwarded-for":"2.2.2.2"}'`
 echo "Create session 1: $RESULT"
 
@@ -80,14 +90,14 @@ SESSION=`echo $RESULT | jq ".session" | tr -d '"'`
 echo $SESSION
 
 RESULT=`curl -s http://localhost:3000/v1/session/email/test@paranoidguy.com -XPOST \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json" \
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json" \
    -d '{"clientip":"1.1.1.1","x-forwarded-for":"2.2.2.2","info":"email"}'`
 echo "Create session 2: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/session/session/$SESSION \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "Get session 1: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/session/phone/4444 \
-   -H "X-Bunker-Token: "$DATABUNKER_APIKEY -H "Content-Type: application/json"`
+   -H "X-Bunker-Token: "$XTOKEN -H "Content-Type: application/json"`
 echo "Get sessions: $RESULT"
