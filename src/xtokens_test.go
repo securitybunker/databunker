989c887d9dd29073fecc3f7ce5f0409c2ffcd8f1
Yuli <stremovsky@gmail.com>
2020-01-20 22:52:09 +0200

    add test to fetch requests

--- src/xtokens_test.go
+++ src/xtokens_test.go
@@ -1,7 +1,6 @@
 package databunker
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http/httptest"
 	//"strconv"
@@ -12,43 +11,29 @@ import (
 func helpCreateUserLogin(mode string, address string) (map[string]interface{}, error) {
 	url := "http://localhost:3000/v1/login/" + mode + "/" + address
 	request := httptest.NewRequest("GET", url, nil)
-	rr := httptest.NewRecorder()
-	request.Header.Set("Content-Type", "application/json")
 	//request.Header.Set("X-Bunker-Token", rootToken)
-
-	router.ServeHTTP(rr, request)
-	var raw map[string]interface{}
-	fmt.Printf("Got: %s\n", rr.Body.Bytes())
-	err := json.Unmarshal(rr.Body.Bytes(), &raw)
-	return raw, err
+	return helpServe(request)
 }
 
 func helpGetUserRequests() (map[string]interface{}, error) {
 	url := "http://localhost:3000/v1/requests"
 	request := httptest.NewRequest("GET", url, nil)
-	rr := httptest.NewRecorder()
-	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("X-Bunker-Token", rootToken)
+	return helpServe(request)
+}
 
-	router.ServeHTTP(rr, request)
-	var raw map[string]interface{}
-	fmt.Printf("Got: %s\n", rr.Body.Bytes())
-	err := json.Unmarshal(rr.Body.Bytes(), &raw)
-	return raw, err
+func helpApproveUserRequest(rtoken string) (map[string]interface{}, error) {
+	url := "http://localhost:3000/v1/request/" + rtoken
+	request := httptest.NewRequest("POST", url, nil)
+	request.Header.Set("X-Bunker-Token", rootToken)
+	return helpServe(request)
 }
 
 func helpCreateUserLoginEnter(mode string, address string, code string) (map[string]interface{}, error) {
 	url := "http://localhost:3000/v1/enter/" + mode + "/" + address + "/" + code
 	request := httptest.NewRequest("GET", url, nil)
-	rr := httptest.NewRecorder()
-	request.Header.Set("Content-Type", "application/json")
 	//request.Header.Set("X-Bunker-Token", rootToken)
-
-	router.ServeHTTP(rr, request)
-	var raw map[string]interface{}
-	fmt.Printf("Got: %s\n", rr.Body.Bytes())
-	err := json.Unmarshal(rr.Body.Bytes(), &raw)
-	return raw, err
+	return helpServe(request)
 }
 
 func TestUserLoginDelete(t *testing.T) {
@@ -83,8 +68,7 @@ func TestUserLoginDelete(t *testing.T) {
 		}
 	*/
 	raw3, _ := helpCreateUserLoginEnter("email", email, "4444") //strconv.Itoa(int(tmpCode)))
-	status = raw3["status"].(string)
-	if status == "error" {
+	if raw3["status"].(string) == "error" {
 		t.Fatalf("Failed to create user login: %s", raw3["message"].(string))
 	}
 	xtoken := raw3["xtoken"].(string)
@@ -92,16 +76,41 @@ func TestUserLoginDelete(t *testing.T) {
 	oldRootToken := rootToken
 	rootToken = xtoken
 	raw4, _ := helpGetUserAppList(userTOKEN)
-	status = raw4["status"].(string)
-	if status == "error" {
+	if raw4["status"].(string) == "error" {
 		t.Fatalf("Failed to get user app list with user xtoken\n")
 	}
 	fmt.Printf("apps: %s\n", raw4["apps"])
 	// user asks to forget-me
-	//helpDeleteUser("token", userTOKEN)
-
+	raw5, _ := helpDeleteUser("token", userTOKEN)
+	if raw5["status"].(string) != "ok" {
+		t.Fatalf("Failed to delete user")
+	}
+	if raw5["result"].(string) != "request-created" {
+	}
+	rtoken0 := raw5["rtoken"].(string)
 	rootToken = oldRootToken
+	// get user requests
+	raw6, _ := helpGetUserRequests()
+	if raw6["total"].(float64) != 1 {
+		t.Fatalf("Wrong number of audit event/s\n")
+	}
+	records := raw6["rows"].([]interface{})
+	records0 := records[0].(map[string]interface{})
+	rtoken := records0["rtoken"].(string)
+	if len(rtoken) == 0 {
+		t.Fatalf("Failed to extract request token\n")
+	}
+	if rtoken != rtoken0 {
+		t.Fatalf("Rtoken0 is wrong\n")
+	}
+	fmt.Printf("** User request record: %s\n", rtoken)
 	helpCreateUserApp(userTOKEN, "qq", `{"custom":1}`)
-	raw5, _ := helpGetUserAppList(userTOKEN)
-	fmt.Printf("apps: %s\n", raw5["apps"])
+	raw7, _ := helpGetUserAppList(userTOKEN)
+	fmt.Printf("apps: %s\n", raw7["apps"])
+	helpApproveUserRequest(rtoken)
+	// user should be deleted now
+	raw8, _ := helpGetUserAppList(userTOKEN)
+	if raw8["apps"] != nil {
+		t.Fatalf("Apps shoud be nil\n")
+	}
 }
