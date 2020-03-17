fe532510d25596dc86927a9f39bc1bdf935fd59f
Yuli <stremovsky@gmail.com>
2020-03-17 13:53:02 +0200

    change consent

--- create-test-user.sh
+++ create-test-user.sh
@@ -11,7 +11,8 @@ RESULT=`curl -s http://localhost:3000/v1/consent/email/test@paranoidguy.com/cont
 echo "Create anonymous consent by email 1: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/email/test@paranoidguy.com/contract-approval -XPOST \
-   -H "X-Bunker-Token: $XTOKEN"`
+   -H "X-Bunker-Token: $XTOKEN"` \
+   -d '{"lawfulbasis":"contract"}'
 echo "Create anonymous consent by email 2: $RESULT"
 
 RESULT=`curl -s http://localhost:3000/v1/consent/phone/4444/contract-approval -XPOST \
