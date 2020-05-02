4d16959971caa8109c5f553d7b882f2530384240
Yuli <stremovsky@gmail.com>
2020-05-03 01:22:15 +0300

    display user friendly alert messages

--- ui/site/site.js
+++ ui/site/site.js
@@ -63,3 +63,22 @@ function shortString(value, row, index) {
   }
   return "<span title='" + value + "' class='shortstring'>" + value + "</span>";
 }
+
+function showAlert(message) {
+	var alertModal =
+    $('<div class="modal fade" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content">' +
+      '<div class="modal-header">' +
+      '<h5 class="modal-title">Alert</h5>' +
+      '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+      '<span aria-hidden="true">&times;</span></button>' +
+      '</div>' +
+      '<div class="modal-body">' +
+         '<div class="alert alert-warning" role="alert">' + message + '</div>' +
+      '</div>' +
+      '<div class="modal-footer">' +
+        '<a href="#" class="btn" data-dismiss="modal">OK</a>' +
+      '</div>' +
+      '</div></div></div>');
+
+    alertModal.modal('show');
+}
