f27c454f29896019e721230e476bf812af738059
Yuli <stremovsky@gmail.com>
2020-05-19 20:05:18 +0300

    use short message

--- src/users_api.go
+++ src/users_api.go
@@ -353,7 +353,7 @@ func (e mainEnv) userLogin(w http.ResponseWriter, r *http.Request, ps httprouter
 			returnError(w, r, "internal error", 405, err, event)
 			return
 		}
-		event.Msg = "Generating access token: " + hashedToken
+		event.Msg = "Generate: " + hashedToken
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(200)
 		fmt.Fprintf(w, `{"status":"ok","xtoken":"%s","token":"%s"}`, xtoken, userTOKEN)
