ddfae137b28ac9bbf3867542b4b57a3a5522f32e
Yuli <stremovsky@gmail.com>
2020-07-01 22:24:37 +0300

    adding admin privacy settings page

--- ui/site/site.js
+++ ui/site/site.js
@@ -82,3 +82,21 @@ function showAlert(message) {
 
     alertModal.modal('show');
 }
+
+function showForm(title, message) {
+	var formModal =
+    $('<div class="modal fade" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content">' +
+      '<div class="modal-header">' +
+      '<h5 class="modal-title">' + title + '</h5>' +
+      '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+      '<span aria-hidden="true">&times;</span></button>' +
+      '</div>' +
+      '<div class="modal-body">' +
+         message +
+      '</div>' +
+      '<div class="modal-footer">' +
+        '<a href="#" class="btn" data-dismiss="modal">Cancel</a>' +
+      '</div>' +
+      '</div></div></div>');
+    formModal.modal('show');
+}
