0862fe954ae979a84c9ba683ec5551ccc26f0023
root <stremovsky@gmail.com>
2020-08-05 22:22:39 +0000

    update menu

--- ui/site/conf.js
+++ ui/site/conf.js
@@ -1,7 +1,6 @@
 const adminMenu = [
   {name: "User requests", file: "admin-view-requests.html"},
   {name: "Privacy settings", file: "admin-privacy-settings.html"},
-  {name: "Magic Sync", file: "admin-magic-sync.html"},
   {name: "Audit", file: "admin-audit.html"},
   {name: "Logout", file: "javascript:bunker_logout();"}
 ];
