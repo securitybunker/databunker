341f3dbe12e5347cc3c9ae4a66538cb81e59667f
root <root@databunker.org>
2021-05-14 12:23:14 +0000

    rename user-consent-management.html to user-agreements.html

--- ui/site/conf.js
+++ ui/site/conf.js
@@ -8,7 +8,7 @@ const adminMenu = [
 const userMenu = [
   {name: "Profile", file: "user-profile.html"},
   {name: "App data", file: "user-apps.html"},
-  {name: "Privacy control", file: "user-consent-management.html"},
+  {name: "My agreements", file: "user-agreements.html"},
   {name: "My requests", file: "user-requests.html"},
   {name: "History", file: "user-audit.html"},
   {name: "Logout", file: "javascript:bunker_logout();"}
