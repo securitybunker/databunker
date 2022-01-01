fa0d3429a9372cdfc14fb537d7b16217cc485518
stremovsky <stremovsky@gmail.com>
2022-01-01 18:51:59 +0200

    update variables

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,6 +1,8 @@
 
 provider "aws" {
-  region = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 
 locals {
--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+default = "eu-north-1"
+}
+
 resource "random_password" "db_password" {
   length           = 16
   special          = true
