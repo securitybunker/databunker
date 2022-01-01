a51b3606c5191b4b465e2588cb89385ff9cc3acf
root <root@databunker.org>
2022-01-01 17:24:58 +0000

    fix variables

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,5 +1,9 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+default = ""
+}
+variable "aws_secret_key" {
+default = ""
+}
 variable "region" {
 default = "eu-north-1"
 }
