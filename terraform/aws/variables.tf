3a068bfa7d4629ba07ac9e0bf68ec4ce44ae9979
stremovsky <stremovsky@gmail.com>
2022-01-04 14:08:59 +0200

    update terraform variables

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,11 +1,44 @@
+
+variable "name_suffix" {
+  type = string
+  default = "databunker"
+}
+
 variable "aws_access_key" {
-default = ""
+  type = string
+  default = ""
 }
+
 variable "aws_secret_key" {
-default = ""
+  type = string
+  default = ""
 }
-variable "region" {
-default = "eu-north-1"
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "resource_tags" {
+  description = "Tags to set for all resources"
+  type        = map(string)
+  default     = {
+    project     = "databunker",
+    environment = "dev"
+  }
+}
+
+variable "ec2_eks_instance_type" {
+  description = "AWS EC2 instance type for EKS nodes."
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ec2_rds_instance_type" {
+  description = "AWS EC2 instance type for RDS nodes."
+  type        = string
+  default     = "db.t3.medium"
 }
 
 resource "random_password" "db_password" {
