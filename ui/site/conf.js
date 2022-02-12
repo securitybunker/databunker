99e18a7f1f4a591f817728904901780d79381d23
root <root@databunker.org>
2022-02-12 18:11:36 +0000

    rename links

--- ui/site/conf.js
+++ ui/site/conf.js
@@ -8,7 +8,7 @@ const adminMenu = [
 const userMenu = [
   {name: "Profile", file: "user-profile.html"},
   {name: "App data", file: "user-apps.html"},
-  {name: "My agreements", file: "user-agreements.html"},
+  {name: "Consent tool", file: "user-agreements.html"},
   {name: "My requests", file: "user-requests.html"},
   {name: "History", file: "user-audit.html"},
   {name: "Logout", file: "javascript:bunker_logout();"}
