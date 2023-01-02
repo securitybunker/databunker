b45ff28621ee24ad35b1d4e102447616c372a8c5
yuli <stremovsky@gmail.com>
2023-01-02 18:03:27 +0200

    fix script to start databunker container

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker-compose down || true
 docker build -t securitybunker/databunker:latest .
-docker-compose up -d
+docker-compose -f docker-compose-pgsql.yml down || true
+docker-compose -f docker-compose-pgsql.yml up -d
