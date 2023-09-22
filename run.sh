89bc776801c5966bb46570dd96ddce0c835c1506
praveen-livspace <praveen@livspace.com>
2023-09-22 17:46:44 +0530

    Update run script

--- run.sh
+++ run.sh
@@ -1,37 +1,9 @@
 #!/bin/sh
 
-#/bin/busybox find /databunker
-
-if [ ! -f /databunker/data/databunker.db ]; then
-  OPTION="-init"
-  if [ "$1" == "demo" ]; then
-    OPTION="-demoinit"
-  fi
-  echo "-------------INIT------------"
-  #/bin/busybox mkdir -p /tmp
-  RESULT=`/databunker/bin/databunker $OPTION -db /databunker/data/databunker.db -conf /databunker/conf/databunker.yaml > /tmp/init.txt`
-  if [ ! -f /databunker/data/databunker.db ]; then
-    echo "Failed to init databunker database. Probably permission issue for /databunker/data directory."
-    /bin/busybox sleep 60
-    exit
-  fi
-  DATABUNKER_ROOTTOKEN=`/bin/busybox awk '/API Root token:/ {print $4}' /tmp/init.txt`
-  DATABUNKER_MASTERKEY2=`/bin/busybox awk '/Master key:/ {print $3}' /tmp/init.txt`
-  echo "DATABUNKER_ROOTTOKEN $DATABUNKER_ROOTTOKEN"
-  echo "DATABUNKER_MASTERKEY $DATABUNKER_MASTERKEY2"
-  /bin/busybox rm -rf /tmp/init.txt
-  if [ -z "$DATABUNKER_MASTERKEY" ]; then
-    export DATABUNKER_MASTERKEY=$DATABUNKER_MASTERKEY2
-  fi
-fi
 if [ -z "$DATABUNKER_MASTERKEY" ]; then
   echo "DATABUNKER_MASTERKEY environment value is empty"
   /bin/busybox sleep 60
   exit
 fi
-#echo "-------------ENV-------------"
-#/bin/busybox env
-#echo "-------------FIND------------"
-#/bin/busybox find /databunker
 echo "-------------RUN-------------"
-/databunker/bin/databunker -start -db /databunker/data/databunker.db -conf /databunker/conf/databunker.yaml
+/databunker/bin/databunker -start -conf /databunker/conf/databunker.yaml
