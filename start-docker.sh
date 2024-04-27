c66bc511c05378817a0597e30dc08609abfce2b3
Yuli <stremovsky@gmail.com>
2024-04-28 00:21:36 +0300

    fix script

--- start-docker.sh
+++ start-docker.sh
@@ -2,5 +2,6 @@
 
 VERSION=$(cat ./version.txt)
 docker build -t securitybunker/databunker:$VERSION --build-arg VERSION=$VERSION .
+docker tag securitybunker/databunker:$VERSION securitybunker/databunker:latest
 docker-compose -f docker-compose-pgsql.yml down || true
 docker-compose -f docker-compose-pgsql.yml up -d
