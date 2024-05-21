fae0817cd6cc4fac5616729afe146f4dbcf56348
Yuli <stremovsky@gmail.com>
2024-05-21 19:48:31 +0300

    update file

--- start-docker.sh
+++ start-docker.sh
@@ -3,5 +3,5 @@
 VERSION=$(cat ./version.txt)
 docker build -t securitybunker/databunker:$VERSION .
 docker tag securitybunker/databunker:$VERSION securitybunker/databunker:latest
-docker-compose -f docker-compose-pgsql.yml down || true
-docker-compose -f docker-compose-pgsql.yml up -d
+docker compose -f docker-compose-pgsql.yml down || true
+docker compose -f docker-compose-pgsql.yml up -d
