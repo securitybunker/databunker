6541a16f32c4b7c50dd432c911df0d0fe8743833
Yuli <stremovsky@gmail.com>
2024-08-07 10:19:19 +0300

    remove build commands

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-VERSION=$(cat ./version.txt)
-docker build -t securitybunker/databunker:$VERSION .
-docker tag securitybunker/databunker:$VERSION securitybunker/databunker:latest
 docker compose -f docker-compose-pgsql.yml down || true
 docker compose -f docker-compose-pgsql.yml up -d
